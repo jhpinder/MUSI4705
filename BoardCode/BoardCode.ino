3c6f1236d3a8b393879410423469f2929ea3a23d
Jared Henson <jaredhenson404@gmail.com>
2019-04-10 15:14:08 -0400

    Update BoardCode.ino

--- BoardCode/BoardCode.ino
+++ BoardCode/BoardCode.ino
@@ -1,13 +1,24 @@
+#include <Adafruit_NeoPixel.h>
+
 //board pin arrays
 int majPins[][8] ={ {2,8,22,28,34,40,46,52},
-                    {4,10,24,30,36,42,48,14},
-                    {6,12,26,32,38,44,50,16},
+                    {4,10,24,30,36,42,48,A10},
+                    {6,12,26,32,38,44,50,A12},
                   };
 int minPins[][8] ={ {3,9,23,29,35,41,47,53},
-                    {5,11,25,31,37,43,49,15},
-                    {7,13,27,33,39,45,51,17},
+                    {5,11,25,31,37,43,49,A9},
+                    {7,13,27,33,39,45,51,A11},
                   };
                  
+//Adafruit_NeoPixel pix0 = Adafruit_NeoPixel(7, A12, NEO_RGB + NEO_KHZ800);
+Adafruit_NeoPixel pix1 = Adafruit_NeoPixel(8, A13, NEO_RGB + NEO_KHZ800);
+Adafruit_NeoPixel pix2 = Adafruit_NeoPixel(8, A14, NEO_RGB + NEO_KHZ800);
+Adafruit_NeoPixel pix3 = Adafruit_NeoPixel(8, A15, NEO_RGB + NEO_KHZ800);
+
+uint32_t maj3 = pix1.Color(0,255,0);
+uint32_t min3 = pix1.Color(0,0,255);
+
+int colNo = 8 ;
 
 void setup() {
 
@@ -31,40 +42,88 @@ for (int thisminRow=0; thisminRow <3; thisminRow++) {
     //Serial.println(" activated!");
   }
 }
+//initialize NeoPixel pins
+//pinMode(A12,OUTPUT);
+pinMode(A13,OUTPUT);
+pinMode(A14,OUTPUT);
+pinMode(A15,OUTPUT);
 
+//pix0.begin();
+//pix0.setBrightness(50);
+//pix0.show();
+pix1.begin();
+pix1.setBrightness(50);
+pix1.show();
+pix2.begin();
+pix2.setBrightness(50);
+pix2.show();
+pix3.begin();
+pix3.setBrightness(50);
+pix3.show();
 
 }
 
 void loop() {
 
 //for loop to sequence one pass through 8 steps
-//for (int seqNo = 0; seqNo < 2;) {
-  int seqNo=0;
+for (int seqNo = 0; seqNo < colNo; seqNo++) {
   int currentRoot = 60;
-  if (seqNo == 0) {
-    int currentRoot = 60;//could be variable with input device
+  //ohmmeter variables
+  int Vin=5;
+  float Vout=0;
+  float R1=470;//knownvalue
+  float R2=0;//testvalue
+  int a2d_data=0;
+  float buffer=0;
+    
+  //resistorID code
+  a2d_data=analogRead(seqNo-1);//will need to be replaced with appropriate pin
+  buffer=a2d_data*Vin;
+  Vout=(buffer)/1024.0;
+  buffer=Vout/(Vin-Vout); 
+  R2=R1*buffer;
+  
+  if (R2 > 40 && R2 < 54) //1
+  {
+    currentRoot = 60;
+  }
+  
+  else if (R2 > 140 && R2 < 160) //2
+  {
+    currentRoot = 62;
   }
 
-  else {
-    
-    //ohmmeter variables
-    int Vin=5;
-    float Vout=0;
-    float R1=2200;//knownvalue
-    float R2=0;//testvalue
-    int a2d_data=0;
-    float buffer=0;
-    
-    //resistorID code
-    a2d_data=analogRead(A0);//will need to be replaced with appropriate pin
-    buffer=a2d_data*Vin;
-    Vout=(buffer)/1024.0;
-    buffer=Vout/(Vin-Vout); 
-    R2=R1*buffer;
-    
-    //Serial.println(R2);
-
+  else if (R2 > 258 && R2 < 275) //3
+  {
+    currentRoot = 64;
   }
+
+  else if (R2 > 318 && R2 < 330) //4
+  {
+    currentRoot = 65;
+  }
+
+  else if (R2 > 500 && R2 < 515) //5
+  {
+    currentRoot = 67;
+  }
+
+  else if (R2 > 665 && R2 < 685) //6
+  {
+    currentRoot = 69;
+  }
+
+  else if (R2 > 1900 && R2 < 2100) //7
+  {
+    currentRoot = 71;
+  }
+
+  else if (R2 > 480000)
+  {
+    currentRoot = 60;
+  }
+
+ 
 
   
   //array containing binary read from columns, laid out maj,min in the order they appear on the board
@@ -100,22 +159,68 @@ void loop() {
 
  
     }
+  //NeoPixel time
+  if (seqNo == 0) { //turning off last col of pixels
+    //pix0.setPixelColor(colNo - 1,0,0,0);
+    pix1.setPixelColor(colNo - 1,0,0,0);
+    pix2.setPixelColor(colNo - 1,0,0,0);
+    pix3.setPixelColor(colNo - 1,0,0,0);
+  } else {
+    //pix0.setPixelColor(seqNo - 1,0,0,0);
+    pix1.setPixelColor(seqNo - 1,0,0,0);
+    pix2.setPixelColor(seqNo - 1,0,0,0);
+    pix3.setPixelColor(seqNo - 1,0,0,0);
+  }
+
+  //turning on this col of pixels
+  if (intervalArr[0] == 3) {
+    pix1.setPixelColor(seqNo,min3);
+  } else if (intervalArr[0] == 4) {
+    pix1.setPixelColor(seqNo,maj3);
+  } else if (intervalArr[0] == 0) {
+    pix1.setPixelColor(seqNo,240,255,200);
+  }
+  
+  if (intervalArr[1] == 3) {
+    pix2.setPixelColor(seqNo,min3);
+  } else if (intervalArr[1] == 4) {
+    pix2.setPixelColor(seqNo,maj3);
+  } else if (intervalArr[1] == 0) {
+    pix2.setPixelColor(seqNo,240,255,200);
+  }
+
+  if (intervalArr[2] == 3) {
+    pix3.setPixelColor(seqNo,min3);
+  } else if (intervalArr[2] == 4) {
+    pix3.setPixelColor(seqNo,maj3);
+  } else if (intervalArr[2] == 0) {
+    pix3.setPixelColor(seqNo,240,255,200);
+  }
+  
+  //pix0.show();
+  pix1.show();
+  pix2.show();
+  pix3.show();
+  
   Serial.println(output);
   delay(500);
     
     
                          
-
-
-//} 
-
-
-
-
 }
 
 
+} 
+
+
+
+
+
+
+
   
+
+
 
 
 
